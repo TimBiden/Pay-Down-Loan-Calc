2ce6bd2142f09fa3acf50e21a2dc8711424e076a
Tim Biden <timbiden@gmail.com>
2019-12-02 12:53:15 -0800

    Interest & Payment work.

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
 const http = require("http");
-const LoanSchedule = require('./index');
+const LoanSchedule = require("./index");
 
-const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end(
-        let loanSchedule = new LoanSchedule({
-            decimalDigit: 2,
-            dateFormat: 'DD.MM.YYYY',
-            prodCalendar: "ru"
-        })
-    );
-
-        let interest = loanSchedule.calculateInterestByPeriod({
-            from: '10.12.2015',
-            to: '10.01.2016',
-            amount: 1000,
-            rate: 16.7
-        });
-    );
+let loanSchedule = new LoanSchedule({
+	decimalDigit: 2,
+	dateFormat: "DD.MM.YYYY",
+	prodCalendar: "ru"
 });
 
-server.listen(port, hostname, () => {
-    console.log(interest);
+let interest = loanSchedule.calculateInterestByPeriod({
+	from: "10.12.2015",
+	to: "10.01.2016",
+	amount: 1000,
+	rate: 16.7
+});
+
+let payment = loanSchedule.calculateAnnuityPaymentAmount({
+	amount: 110000,
+	term: 60,
+	rate: 12.9
+});
+
+let schedule = loanSchedule.calculateSchedule({
+	amount: 50000,
+	rate: 11.5,
+	term: 12,
+	paymentOnDay: 25,
+	issueDate: "25.10.2016",
+	scheduleType: loanSchedule.ANNUITY_SCHEDULE
+});
+
+const requestHandler = () => {
+	schedule.response.end(payments.forEach(function (pay) {console.log(pay.paymentDate + "\t|\t\t" + pay.initialBalance + "\t|\t\t" + pay.paymentAmount + "\t|\t\t" + pay.principalAmount + "\t|\t\t" + pay.interestAmount + "\t|\t\t" + pay.finalBalance);
+	})
+	);
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+	if (err) {
+		return console.log("something bad happened", err);
+	}
 });
